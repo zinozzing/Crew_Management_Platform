c808ee1c4319e1c2dcbd5026e744e5403571d3cd
zinozzing <rlawlsgh1111@naver.com>
2023-01-13 16:07:51 +0900

    ~Header, Init

---
+++ frontend/app.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <header class="w-screen h-24 border-b-2 flex items-center">
+      <Icon name="uil:github" class="flex items-center ml-5" />
+      <div class="tab flex-auto flex justify-end">
+        <a href="" v-for="(tab, i) in tabs" :key="i">{{ tab }}</a>
+      </div>
+    </header>
+    <nav></nav>
+    <main></main>
+    <footer></footer>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      tabs: [
+        "게시판",
+        "회계내역확인",
+        "설문조사",
+        "실시간채팅",
+        "회원관리",
+        "회원가입/로그인",
+      ],
+    };
+  },
+};
+</script>
+<style>
+.tab {
+  padding: 15px;
+  border-radius: 5px;
+}
+.tab a {
+  padding: 10px;
+  text-decoration: none;
+}
+</style>
